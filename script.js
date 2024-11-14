2d2a8b378bf1f55af53de0f89ae70f78be24fafe
RacheleBurgio <rachele.burgio32@gmail.com>
2024-11-14 16:33:19 +0100

    inseriti ids al posto di 9mins

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const imgNew = document.getElementsByClassName(
   'bd-placeholder-img card-img-top'
 )
 const cards = document.querySelectorAll('.card')
-
+const ids = document.querySelectorAll('.text-muted')
 const API_KEY = '5rOfqg3sEG6IqSidyB26BBiZVwur4nqaCvrhpCS2IcI6TA26RWkIzEY1'
 const URLKittens = 'https://api.pexels.com/v1/search?query=Kittens'
 const URLBooks = 'https://api.pexels.com/v1/search?query=Books'
@@ -26,16 +26,26 @@ const fetchImg1 = () => {
       }
     })
     .then((photo) => {
-      for (let i = 0; i < photo.photos.length && i < imgNew.length; i++) {
+      for (
+        let i = 0;
+        i < photo.photos.length && i < imgNew.length && i < ids.length;
+        i++
+      ) {
         imgNew[i].setAttribute('src', photo.photos[i].src.landscape)
+
+        if (ids[i]) {
+          ids[i].textContent = photo.photos[i].id
+        } else {
+          console.log(
+            `Elemento con nome 'text-muted' non trovato per l'indice ${i}`
+          )
+        }
       }
-      console.log('photo', photo.photos[1].src.landscape)
     })
     .catch((errore) => {
       console.log(errore)
     })
 }
-
 const fetchImg2 = () => {
   fetch(URLBooks, {
     method: 'GET',
@@ -53,8 +63,20 @@ const fetchImg2 = () => {
       }
     })
     .then((photo) => {
-      for (let i = 0; i < photo.photos.length && i < imgNew.length; i++) {
+      for (
+        let i = 0;
+        i < photo.photos.length && i < imgNew.length && i < ids.length;
+        i++
+      ) {
         imgNew[i].setAttribute('src', photo.photos[i].src.landscape)
+
+        if (ids[i]) {
+          ids[i].textContent = photo.photos[i].id
+        } else {
+          console.log(
+            `Elemento con nome 'text-muted' non trovato per l'indice ${i}`
+          )
+        }
       }
     })
     .catch((errore) => {
